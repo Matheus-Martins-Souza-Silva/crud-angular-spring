035f6b0f9f54109a36b37a4a66b6d65c73a92dfa
Matheus Martins <martinsmatheus066@gmail.com>
2024-04-12 19:35:46 -0300

    Old Angular

---
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -0,0 +1,90 @@
+import { Location } from '@angular/common';
+import { Component, Pipe } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, UntypedFormArray, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+
+import { CoursesService } from '../../services/courses.service';
+import { Course } from '../../models/course';
+import { Lesson } from '../../models/lesson';
+import { FormUtilsService } from 'src/app/shared/form/form-utils.service';
+
+@Component({
+  selector: 'app-course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.scss']
+})
+export class CourseFormComponent {
+
+  form!: FormGroup;
+
+  constructor(private formBuilder: NonNullableFormBuilder, private service: CoursesService, private _snackBar: MatSnackBar,
+    private location: Location, private route: ActivatedRoute, public formUtils: FormUtilsService) {
+
+      const course: Course = this.route.snapshot.data['course'];
+
+        this.form = this.formBuilder.group({
+          _id: [course._id],
+          name: [course.name, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+          category: [course.category, [Validators.required]],
+          lessons: this.formBuilder.array(this.retrieveLessons(course), Validators.required)
+        });
+  }
+
+  private retrieveLessons(course: Course) {
+
+    const lessons = [];
+
+    if(course?.lessons) {
+      course.lessons.forEach(lesson => lessons.push(this.createLesson(lesson)));
+    } else {
+      lessons.push(this.createLesson());
+    }
+
+    return lessons;
+  }
+
+  private createLesson(lesson: Lesson = { id: '', name: '', youtubeUrl: '' }) {
+    return this.formBuilder.group({
+      id: [lesson.id],
+      name: [lesson.name, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      youtubeUrl: [lesson.youtubeUrl, [Validators.required, Validators.minLength(10), Validators.maxLength(11)]]
+    });
+  }
+
+  getLessonsFormArray() {
+    return (<UntypedFormArray>this.form.get('lessons')).controls;
+  }
+
+  addNewLesson() {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    lessons.push(this.createLesson());
+  }
+
+  removeLesson(index: number) {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    lessons.removeAt(index);
+  }
+
+  onSubmit() {
+    if(this.form.valid) {
+      this.service.save(this.form.value).subscribe(result => this.onSucess(), error => this.onError());
+    }
+    else {
+      this.formUtils.validateAllFormFields(this.form);
+    }
+  }
+
+  onCancel() {
+    this.location.back();
+  }
+
+  private onError() {
+    this._snackBar.open('Erro ao salvar Curso.', '', {duration: 5000});
+  }
+
+  private onSucess() {
+    this._snackBar.open('Curso salvo com sucesso.', '', {duration: 5000});
+    this.onCancel();
+  }
+}
---
+++ src/app/courses/containers/courses/courses.component.ts
@@ -0,0 +1,69 @@
+import { ConfirmationDialogComponent } from './../../../shared/components/confirmation-dialog/confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Course } from '../../models/course';
+import { Component } from '@angular/core';
+import { CoursesService } from '../../services/courses.service';
+import { Observable, catchError, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent {
+  courses$: Observable<Course[]> | null = null;
+
+  constructor(private courseService: CoursesService, public dialog: MatDialog, private router: Router,
+    private route: ActivatedRoute, private _snackBar: MatSnackBar) {
+    this.refresh();
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  onEdit(course: Course) {
+    this.router.navigate(['edit', course._id], {relativeTo: this.route});
+  }
+
+  onRemove(course: Course) {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: 'Tem certeza que deseja remover esse curso?',
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if(result) {
+        this.courseService.remove(course._id).subscribe(
+          () => {
+            this.refresh();
+            this._snackBar.open('Curso removido com sucesso.', 'X', {
+              duration: 5000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center'
+            });
+          },
+          () => this.onError("Erro ao tentar remover curso.")
+        )
+      }
+    });
+  }
+
+  refresh() {
+    this.courses$ = this.courseService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar o curso.');
+        return of([])
+      })
+    );
+  }
+}
